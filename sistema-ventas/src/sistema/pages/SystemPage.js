d38a43fa3420673a2d3b617715f67877c7b56bf7
EdisonAlzate <edisonalzate11@gmail.com>
2023-03-14 22:19:29 -0500

    Se le agrego el boton agregar

--- sistema-ventas/src/sistema/pages/SystemPage.js
+++ sistema-ventas/src/sistema/pages/SystemPage.js
@@ -1,5 +1,7 @@
 
 
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { SystemLayout } from "../layout/SystemLayout"
 import { NoteView } from "../views"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -7,9 +9,23 @@ import { NothingSelectedView } from "../views/NothingSelectedView"
 export const SystemPage = () => {
   return (
     <SystemLayout>
+      <NothingSelectedView />
       {/*<NothingSelectedView />*/}
-      <NoteView/>
+      {/*<NoteView/>*/}
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </SystemLayout>
   )
 }
- 
+
